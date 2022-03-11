ebfe7f9f8ac40dece0e53ddea10ff6691f179359
李东哲 <lidongzhe@nightingales.cn>
2022-03-11 13:03:51 +0800

    - buf

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MainPage from './pages/ts/MainPage';
+import Navigator from './components/ts/Navigator';
 function App() {
+  const tags = [
+    { name: "首页", url: "/home" },
+    { name: "DGoDo", url: "http://dgodo.dggua.top" },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigator tags={tags}></Navigator>
+      <MainPage></MainPage>
+    </>
   );
 }
 
