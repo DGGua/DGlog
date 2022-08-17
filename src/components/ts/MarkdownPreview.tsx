9e350a5e71d387cf8df09387344e885e4838f481
DG_Gua <DG_Gua@foxmail.com>
2022-08-17 23:57:38 +0800

    add code highlight

--- src/components/ts/MarkdownPreview.tsx
+++ src/components/ts/MarkdownPreview.tsx
@@ -1,9 +1,16 @@
 import { marked } from "marked";
 import { useEffect, useRef } from "react";
+import hljs from "highlight.js";
 import "../scss/MarkdownPreview.scss";
 export interface MarkdownPreviewProps {
   content: string;
 }
+marked.use({
+  highlight: (code, lang) => {
+    console.log(hljs.highlightAuto(code, [lang]).value);
+    return hljs.highlightAuto(code, [lang]).value;
+  },
+});
 export default function MarkdownPreview(props: MarkdownPreviewProps) {
   const textelement = useRef<HTMLDivElement>(null);
   useEffect(() => {
